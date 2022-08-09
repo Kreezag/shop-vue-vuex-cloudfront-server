0040353bbd0d9c63de3f5bb4ff0aa411f0d06c3b
Kuchuk Andrey <kreezag@gmail.com>
2022-08-09 10:09:17 +0400

    move credentials to env file

--- product-service/src/libs/dbConnection.ts
+++ product-service/src/libs/dbConnection.ts
@@ -1,20 +1,14 @@
 import { Pool } from 'pg';
-// import dotenv from 'dotenv'
-// dotenv.config()
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-
-let DATABASE_HOST='cloudfront-database-instance.cpds6klib029.eu-west-1.rds.amazonaws.com',
-DATABASE_PORT=5432,
-USER_NAME='postgres',
-DATABASE_PASSWORD='fK7er7Y0eRTFsVYHyEvP',
-DATABASE_NAME='cloudfront_database';
 
 export const createDbConnection = () => {
   return new Pool({
-    host: DATABASE_HOST,
-    port: Number(DATABASE_PORT),
-    user: USER_NAME,
-    database: DATABASE_NAME,
-    password: DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
+    user: process.env.DATABASE_USERNAME,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
   })
 }
